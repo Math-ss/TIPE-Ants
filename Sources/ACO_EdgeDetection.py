c843a1b40752cfae8b22ce27a4c94c601afe6dbc
NootNootP1ngu <raphael.j.maurette@gmail.com>
2023-04-02 13:40:30 +0200

    comm

--- Sources/ACO_EdgeDetection.py
+++ Sources/ACO_EdgeDetection.py
@@ -1,17 +1,73 @@
 import networkx as nx
 import random as rd
+from PIL import Image,ImageOps
+import numpy as np
 
 from ACO_Interface import ACO
 
+
 class EdgeFinder(ACO):
-    def __init__(self, workGraph: nx.Graph) -> None:
+
+    @staticmethod
+    def graphgenerator(nom : str, f : function, neighbourhood : list):
+        """Creates a graph with each node representing a pixel, without including the two bordering rows.
+        f is the fonction used to calculate the contrast of the neighbourhood of each node.
+        neighbourhood has to be a int*int list, for instance following Tian, Yu and Xie definition,
+        neighbourhhod is [(-2,-1),(-2,+1),(-1,2),(-1,-2),(-1,-1),(-1,0),(-1,+1),(0,+1)]"""
+        #1. Creation of numpy array
+        imgpil = Image.open(nom)
+        imggray = ImageOps.grayscale(imgpil) 
+        img = np.array(imggray) #Creates a numpy array of the grayscale encoding of each pixel of imggray
+        ordmax, abscmax = img.shape
+
+        #2. graph initialization and creation of weighted nodes
+        G = nx.DiGraph()
+        
+        def Contrast(x,y) :
+            sum = 0
+            for (i,j) in neighbourhood:
+                sum += abs(img[x+i,y+j]-img[x-i,y-j])
+            return f(sum) #Will vary depending of the function f used
+    
+        def heuristicsum():
+            sum = 0
+            for i in range(2,abscmax-2):
+                for j in range(2,ordmax-2):
+                    sum += Contrast(i,j)
+            return(sum)
+    
+        normalfactor = heuristicsum() #Normalization factor of the graph
+
+        for absciss in range(2,abscmax-2):
+            for ordinate in range(2,ordmax-2):
+                G.add_nodes((absciss,ordinate), heuristic = (Contrast(absciss,ordinate,2,2)/normalfactor))
+        
+        #3. Creation of edges
+
+        for absciss in range(2,abscmax-2):
+            for ordinate in range(2,ordmax-2):
+                liste1 = [(absciss,ordinate),(absciss+i,ordinate+j) for (i,j) in neighbourhood if (absciss+i)<= abscmax-2 and (absciss+i) >= 2 and (ordinate+j) <= ordmax-2 and (ordinate+j) >= 2 ]
+                liste2 = [(absciss,ordinate),(absciss-i,ordinate-j) for (i,j) in neighbourhood if (absciss-i)<= abscmax-2 and (absciss-i) >= 2 and (ordinate-j) <= ordmax-2 and (ordinate-j) >= 2 ]
+                G.add_edges_from(liste1+liste2)
+        
+        G.nodes[(2,2)]["size"] = (abscmax,ordmax)
+
+        return G
+
+    def __init__(self, workGraph: nx.DiGraph) -> None:
         super().__init__(workGraph)
+
+        nx.set_node_attributes(self._graph, 0.5, "pheromone") #Pheromones are stored in the nodes here
+
         #TODO : Initialise everything (cf. online)
+
+        self._absc, self._ord = workGraph.nodes[(2,2)]["size"]
+
         #Heuristic matrix will be stored inside the graph...
 
         # Specific initialisation 
-        self._consecutiveMoves = 40
-        self._antsLocation = [(rd.randrange(0, 1), rd.randrange(0, 1)) for k in range(self._antsByGeneration)] #BUG : Not correct initialisation of position : needs dimensions
+        self._consecutiveMoves = 100
+        self._antsLocation = [(rd.randrange(0, self._absc), rd.randrange(0, self._ord)) for k in range(self._antsByGeneration)] 
 
     def _SolutionConstruction(self) -> None:
         for ant in range(self._antsByGeneration):
@@ -28,4 +84,21 @@ class EdgeFinder(ACO):
 
     def _HeuristicInfo(self, start: int, end: int) -> float:
         return self._graph.nodes[end]["heuristic"]
+    
+    def _CostFunction(self, s: list) -> float:
+        sum = 0.000001 #To avoid zero
+        for i in range(len(s)):
+            sum += self._graph.nodes[s[i]]["heuristic"]
+        return sum
+
+    def _Graphreader(self, Graph : nx.DiGraph, threshold : float) -> None:
+        """Reads the pheromone values on the graph and colors the corresponding pixel if the pheromone level
+        is higher than the given threshold"""
+        (abscmax,ordmax) = Graph.nodes[(2,2)]["size"]
+        imgres = Image.new('RGB',(abscmax,ordmax),"black")
+        for i in range(2,abscmax-2):
+            for j in range(2,ordmax-2):
+                if Graph.nodes[(i,j)]["pheromone"] > threshold :
+                    imgres.putpixel((i,j),(0,0,0))
+        imgres.show()        
 
